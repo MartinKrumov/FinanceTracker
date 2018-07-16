c418a10693e829cf190abf8802edd79d67c67a58
Martin <martin_krumov@abv.bg>
2018-07-16 08:52:43 +0300

    Added category resource

---
+++ src/main/java/com/financetracker/area/rest/CategoryResource.java
@@ -0,0 +1,26 @@
+package com.financetracker.area.rest;
+
+import com.financetracker.area.category.models.CategoryRequestModel;
+import com.financetracker.area.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class CategoryResource {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("/userId/create_category")
+    public ResponseEntity createCategory(@RequestBody CategoryRequestModel newCategory, @PathVariable("userId") Long userId) {
+        categoryService.createCategory(newCategory, userId);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
