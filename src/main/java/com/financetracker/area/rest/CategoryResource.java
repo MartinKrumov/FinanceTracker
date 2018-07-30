61709325d10966a3a0715368ad50e5a8eda151ce
Martin Krumov <martin.krumov@endava.com>
2018-07-30 17:42:29 +0300

    Added SwaggerConfig, edited SpringSecurityConfig, added swagger in pom

--- src/main/java/com/financetracker/area/rest/CategoryResource.java
+++ src/main/java/com/financetracker/area/rest/CategoryResource.java
@@ -2,9 +2,11 @@ package com.financetracker.area.rest;
 
 import com.financetracker.area.category.models.CategoryRequestModel;
 import com.financetracker.area.category.service.CategoryService;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +19,26 @@ public class CategoryResource {
 
     private final CategoryService categoryService;
 
+    @ApiOperation(
+            value = "Create category",
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Proposal is uploaded successfully"),
+            @ApiResponse(code = 400, message = "Validation error. Category with that name already exists"),
+            @ApiResponse(code = 404, message = "User not found")
+    })
+    @ApiImplicitParam(
+            name = "newCategory",
+            value = "The category in JSON format",
+            required = true,
+            dataType = "JSON",
+            paramType = "CategoryRequestModel"
+    )
     @PostMapping("/{userId}/category")
-    public ResponseEntity createCategory(@RequestBody CategoryRequestModel newCategory, @PathVariable Long userId) {
+    public ResponseEntity createCategory(@RequestBody CategoryRequestModel newCategory,
+                                         @ApiParam(name = "userId", value = "The Id of the user creating category")
+                                         @PathVariable Long userId) {
         categoryService.createCategory(newCategory, userId);
 
         return new ResponseEntity(HttpStatus.CREATED);
--- src/main/java/com/financetracker/configuration/SpringSecurityConfig.java
+++ src/main/java/com/financetracker/configuration/SpringSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -40,11 +41,21 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/**",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/register")
-                .permitAll()
+                .antMatchers("/", "/register").permitAll()
+//                .antMatchers("*")
                 .anyRequest()
                 .authenticated()
             .and()
