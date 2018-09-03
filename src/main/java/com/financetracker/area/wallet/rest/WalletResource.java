7dddf5e2711f32f985d75e2dbfa2d112a0ce1139
Martin <martin_krumov@abv.bg>
2018-09-04 01:13:49 +0300

    Testing ObjectMapper

--- src/main/java/com/financetracker/area/wallet/rest/WalletResource.java
+++ src/main/java/com/financetracker/area/wallet/rest/WalletResource.java
@@ -1,15 +1,22 @@
 package com.financetracker.area.wallet.rest;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.financetracker.area.wallet.models.WalletBindingModel;
 import com.financetracker.area.wallet.services.WalletService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Map;
+import java.util.TimeZone;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -22,5 +29,19 @@ public class WalletResource {
         walletService.createWallet(walletBindingModel, userId);
 
         return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/date", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity parse(@RequestBody String json) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, Object> map = objectMapper.readValue(json, new TypeReference<Map<String,Object>>(){});
+
+        long timestamp = (Integer) map.get("timestamp");
+
+        LocalDateTime triggerTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), ZoneId.of("UTC"));
+
+        return ResponseEntity.ok(triggerTime);
     }
 }
