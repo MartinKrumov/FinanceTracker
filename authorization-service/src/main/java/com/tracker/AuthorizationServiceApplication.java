a6620dd1a753d00535a64b02d6ed1f6b7ae4efad
MartinKrumov <martin_krumov@abv.bg>
2019-04-02 20:48:15 +0300

    develop - added additional config for auth service

--- authorization-service/src/main/java/com/tracker/AuthorizationServiceApplication.java
+++ authorization-service/src/main/java/com/tracker/AuthorizationServiceApplication.java
@@ -2,25 +2,11 @@ package com.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@EnableResourceServer
-@EnableAuthorizationServer
 @SpringBootApplication
-@RestController
 public class AuthorizationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServiceApplication.class, args);
-    }
-
-
-    @GetMapping("/take")
-    public ResponseEntity takeAway() {
-        return ResponseEntity.ok("Take it away!");
     }
 }
---
+++ authorization-service/src/main/java/com/tracker/service/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+package com.tracker.service;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return mockUser(username);
+    }
+
+    private UserDetails mockUser(String username) {
+        String userName = "test@test.com";
+        String userPass = "tester";
+
+        if (!userName.equals(username)) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+
+        // this is another way of dealing with password encoding
+        // password will be stored in bcrypt in this example
+        // you can also use a prefix, @see com.patternmatch.oauth2blog.config.AuthorizationServerConfig#CLIENT_SECRET
+
+        return User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(userPass)
+                .authorities(getAuthority())
+                .build();
+    }
+
+    private List<SimpleGrantedAuthority> getAuthority() {
+        return Collections.emptyList();
+    }
+}
