9e78e0f893cc7d0993f3a8f895c54f20a347889d
MartinKrumov <martin_krumov@abv.bg>
2020-02-03 20:44:30 +0200

    gateway - fix initial import for keycloak

--- gateway/src/main/java/com/tracker/rest/UserInfoResource.java
+++ gateway/src/main/java/com/tracker/rest/UserInfoResource.java
@@ -2,8 +2,6 @@ package com.tracker.rest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +13,8 @@ import java.util.Map;
 public class UserInfoResource {
 
     @GetMapping("/userinfo")
-    public ResponseEntity<Map<String, Object>> userInfo(
-            @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-            @AuthenticationPrincipal OAuth2User oauth2User) {
+    public ResponseEntity<Map<String, Object>> userInfo(@AuthenticationPrincipal OAuth2User oauth2User) {
         Map<String, Object> model = new HashMap<>();
-        model.put("clientName", authorizedClient.getClientRegistration().getClientName());
         model.put("userName", oauth2User.getName());
         model.put("userAttributes", oauth2User.getAttributes());
         return ResponseEntity.ok(model);
