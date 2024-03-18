9d3faf6bd842f7988acb096199bbd6506a52017b
Martin <martin_krumov@abv.bg>
2024-03-19 00:10:17 +0200

    ft - add open api 3 and change swagger config

--- uaa-service/src/main/java/com/tracker/config/doc/SwaggerConfig.java
+++ uaa-service/src/main/java/com/tracker/config/doc/SwaggerConfig.java
@@ -1,10 +1,12 @@
 package com.tracker.config.doc;
 
-import org.springframework.beans.factory.annotation.Value;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@EnableSwagger2
 public class SwaggerConfig {
 
     private static final String AUTH_SERVER = "http://localhost:8082/oauth";
@@ -15,103 +17,15 @@ public class SwaggerConfig {
     private static final String LICENSE_URL = "http://www.apache.org/licenses/LICENSE-2.0";
     private static final String VERSION = "1.0.0";
 
-    @Value("${auth.client.client-id}")
-    private String clientId;
-    @Value("${auth.client.client-secret}")
-    private String clientSecret;
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(TITLE)
+                        .description(DESCRIPTION)
+                        .version(VERSION)
+                        .license(new License().name(LICENSE).url(LICENSE_URL))
+                );
+    }
 
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build()
-//                .securitySchemes(List.of(securityScheme()))
-//                .securityContexts(List.of(securityContext()))
-//                .apiInfo(apiInfo());
-//    }
-//
-//    /**
-//     * @return API info for swagger-ui.html
-//     */
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title(TITLE)
-//                .description(DESCRIPTION)
-//                .license(LICENSE)
-//                .licenseUrl(LICENSE_URL)
-//                .version(VERSION)
-//                .build();
-//    }
-//
-//    @Bean
-//    public SecurityConfiguration security() {
-//        return SecurityConfigurationBuilder.builder()
-//                .clientId(clientId)
-//                .clientSecret(clientSecret)
-//                .scopeSeparator(" ")
-//                .useBasicAuthenticationWithAccessCodeGrant(true)
-//                .build();
-//    }
-//
-//    private SecurityScheme securityScheme() {
-//        GrantType grantType = new AuthorizationCodeGrantBuilder()
-//                .tokenEndpoint(new TokenEndpoint(AUTH_SERVER + "/token", "oauthtoken"))
-//                .tokenRequestEndpoint(new TokenRequestEndpoint(AUTH_SERVER + "/authorize", clientId, clientId))
-//                .build();
-//
-//        return new OAuthBuilder()
-//                .name("spring_oauth")
-//                .grantTypes(List.of(grantType))
-//                .scopes(List.of(scopes()))
-//                .build();
-//    }
-//
-//    private SecurityContext securityContext() {
-//        return SecurityContext.builder()
-//                .securityReferences(List.of(new SecurityReference("spring_oauth", scopes())))
-//                .forPaths(PathSelectors.regex("/admin.*"))
-//                .build();
-//    }
-//
-//    private AuthorizationScope[] scopes() {
-//        return new AuthorizationScope[]{
-//                new AuthorizationScope("read", "for read operations"),
-//                new AuthorizationScope("write", "for write operations"),
-//                new AuthorizationScope("trust", "Access FT API")
-//        };
-//    }
-//    @Bean //TODO: Remove nasty hack when springfox fix this
-//    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
-//        return new BeanPostProcessor() {
-//
-//            @Override
-//            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-//                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
-//                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
-//                }
-//                return bean;
-//            }
-//
-//            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
-//                List<T> copy = mappings.stream()
-//                        .filter(mapping -> mapping.getPatternParser() == null)
-//                        .toList();
-//                mappings.clear();
-//                mappings.addAll(copy);
-//            }
-//
-//            @SuppressWarnings("unchecked")
-//            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
-//                try {
-//                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
-//                    field.setAccessible(true);
-//                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
-//                } catch (IllegalArgumentException | IllegalAccessException e) {
-//                    throw new IllegalStateException(e);
-//                }
-//            }
-//        };
-//    }
 }
